44bc9e62696061cc37ae87cf45b87d0fe3f9d455
emmanuelbenson <emmanuel.c.benson@gmail.com>
2025-11-10 00:23:21 +0100

    Add task user relationship, add booted method for primary key uuid generation

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['title', 'user_id', 'description', 'status'];
+
+    protected static function booted(): void
+    {
+        parent::booted();
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
